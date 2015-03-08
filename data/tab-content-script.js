adfbb617b2e82b5f3b73e20f4d6b7830fcbce74f
kfrencher <kfrencher@softwaretheoretic.com>
2015-03-08 18:04:15 -0400

    Added ability to sync to a file on the filesystem called scratch.txt
    Sync is only one way right now. Need to come up with either a watch, or
    a polling mechanism to get the sync to go back the other way. Need to figure
    out a strategy to key the input that is on the webpage to the text currently
    being displayed in the scratch pad because I don't want to mistakenly overwrite
    what's inside the browser. It whould only sync if the correct text is being
    displayed in the browser.

--- data/tab-content-script.js
+++ data/tab-content-script.js
@@ -1,42 +1,62 @@
-var syncValue = function(){
+/**
+ * Flattens the provided collection of arrays into a single array
+ */
+var flatten = function(xss) {
+    return (xss || []).reduce(function(xs, ys) {
+        return xs.concat(ys);
+    }, []);
+};
+
+var calculateFileName = function(input) {
+    return 'scratch.txt';
+
+}
+
+// Sends message to main to sync the data with file
+var emitValue = function(e) {
     console.log('emitting sync event');
-    self.port.emit('sync',{
-        fileName:'scratch.txt',
-        content:this.value
+    self.port.emit('sync', {
+        fileName: calculateFileName(e.target),
+        content: this.value
     });
 }
 
-var observer = new MutationObserver(function(){
-  var getTextInputs = function(doc){
-    if(doc){
-      var inputs = [].concat(
-          Array.from(doc.querySelectorAll('input[type=text]')),
-          Array.from(doc.querySelectorAll('input:not([type])')),
-          Array.from(doc.querySelectorAll('textarea'))
-      );
-      return inputs;
+// Returns all text and textarea inputs in the passed in document
+var getTextInputs = function(doc) {
+    if (doc) {
+        var inputs = [].concat(
+            Array.from(doc.querySelectorAll('input[type=text]')),
+            Array.from(doc.querySelectorAll('input:not([type])')),
+            Array.from(doc.querySelectorAll('textarea'))
+        );
+        return inputs;
     } else {
-      return [];
+        return [];
     }
-  };
-  
-  var flatten = function(xss){
-    return (xss || []).reduce(function(xs, ys){
-      return xs.concat(ys); 
-    }, []);
-  };
-  
-  var textInputsInMainWindow = getTextInputs(document),
-      iframes = Array.from(document.querySelectorAll('iframe')),
-      textInputsInIframes = flatten(iframes.map(function(iframe) iframe.contentDocument)
-                                   .map(getTextInputs)),
-      inputs = [].concat(textInputsInMainWindow, textInputsInIframes);
+};
 
-  
-  inputs.forEach(function(input){
-    console.log('Attaching listener to: '+input.tagName+'[name='+input.name+']');
-    input.addEventListener('change', syncValue);
-  });
+/**
+ * Create an observer that will listen to changes in the body and
+ * search for all inputs on the page. The inputs will then be observed
+ * for changes
+ */
+var observer = new MutationObserver(function() {
+
+    var textInputsInMainWindow = getTextInputs(document),
+        iframes = Array.from(document.querySelectorAll('iframe')),
+        textInputsInIframes = flatten(iframes.map(iframe => iframe.contentDocument)
+            .map(getTextInputs)),
+        inputs = [].concat(textInputsInMainWindow, textInputsInIframes);
+
+
+    inputs.forEach(function(input) {
+        console.log('Attaching listener to: ' + input.tagName + '[name=' + input.name + ']');
+        input.addEventListener('change', emitValue);
+        input.addEventListener('mousedown', emitValue);
+    });
 });
 
-observer.observe(document.body,{childList:true, subtree:true});
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
